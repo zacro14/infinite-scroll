3cc83c14628a06d04d8e4b9fccfeb8f7615797c2
junel <sacrojunel@gmail.com>
2023-02-24 22:44:19 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <div>hello</div>
+      <div className="flex h-screen">
+        <div className="m-auto">
+          <div className="text-6xl text-red-600">{count}</div>
+          <button
+            className="px-6 py-2 rounded bg-green-800 hover:bg-green-600 text-white"
+            type="button"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count+
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
