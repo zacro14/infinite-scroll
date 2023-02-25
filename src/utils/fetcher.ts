e075b7090c8d58e0b3fa7d7be05cd70adf29c694
junel <sacrojunel@gmail.com>
2023-02-25 21:21:29 +0800

    feat: create a utility function as fetcher

---
+++ src/utils/fetcher.ts
@@ -0,0 +1,20 @@
+interface FecthOptions extends RequestInit {
+  json?: boolean;
+}
+
+export async function Fetcher<T>(
+  url: string,
+  options: FecthOptions = {}
+): Promise<T> {
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch ${url}: ${response.status} ${response.statusText}`
+    );
+  }
+
+  if (options.json === false) return (await response.text()) as unknown as T;
+
+  return (await response.json()) as T;
+}
