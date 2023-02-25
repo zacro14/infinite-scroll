a51544d911947f76e5a956600b825add8c10a45b
junel <sacrojunel@gmail.com>
2023-02-25 22:12:38 +0800

    feat: create a reusabale card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-type content = {
+type CardProps = {
   children: React.ReactNode;
+  title: string;
 };
 
-export function Card({ children }: content) {
+export function Card(props: CardProps) {
+  const { title, children } = props;
   return (
-    <div className="bg-gray-100 container mx-auto p-5 rounded-md shadow-lg">
-      {children}
+    <div className="mb-4 bg-gray-100 container mx-auto p-5 rounded-md shadow-lg">
+      <h2 className="text-xl font-bold mb-4">{title}</h2>
+      <div className="text-gray-700">{children}</div>
     </div>
   );
 }
