070c5ca15bfa214312360e34a8703f679162dd90
junel <sacrojunel@gmail.com>
2023-02-28 19:12:59 +0800

    feat : add the about page

---
+++ src/routes/about.tsx
@@ -0,0 +1,11 @@
+import { MainPage } from '@/components/Layout';
+
+const About = () => {
+  return (
+    <MainPage>
+      <div>About Page</div>
+    </MainPage>
+  );
+};
+
+export default About;
