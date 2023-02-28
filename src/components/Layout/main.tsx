f29b677dc076f71d2b3393e91eb0af8f866399ea
junel <sacrojunel@gmail.com>
2023-02-28 19:14:04 +0800

    feat: add header and footer on  layout component for reusbality

--- src/components/Layout/main.tsx
+++ src/components/Layout/main.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Footer from '../Footer';
+import { Header } from '../Header';
 
 export function MainPage({ children }: { children: React.ReactNode }) {
-  return <div className="m-10 max-w-full">{children}</div>;
+  return (
+    <>
+      <Header />
+      <div className="m-10 max-w-full min-h-screen">{children}</div>
+      <Footer />
+    </>
+  );
 }
