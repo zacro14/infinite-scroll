f0327afeafb991f238db6bf5a7a03ea7d55e6b16
junel <sacrojunel@gmail.com>
2023-02-28 19:07:11 +0800

    feat: hero page

---
+++ src/components/Hero/inde.tsx
@@ -0,0 +1,17 @@
+export function Hero() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div>
+          <h1 className="text-5xl font-bold">Space X </h1>
+          <p className="py-6">
+            Welcome to SpaceX - the private aerospace company founded by tech
+            entrepreneur Elon Musk with the goal of making space travel more
+            accessible and sustainable for humanity.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
